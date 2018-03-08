da3556c48b8e47064a9cc7b1339cabedb659f1fb
Dana Palazzo <danapalazzo@Danas-MacBook.local>
2018-03-08 10:54:16 -0500

    Updated bundler

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,5 @@
 class WelcomeController < ApplicationController
   def index
-    render json: {status: 200, message: 'Welcome to the Tweets API!'}
+    render json: {status: 200, message: 'Welcome to the Tweets API, folks!'}
   end
 end
